ff7d5279a3705fa18fdea8ef0d5e6cfc30198082
Oliver K. Ernst <oernst@ucsd.edu>
2021-12-19 19:24:39 -0800

    svg

--- svg.py
+++ svg.py
@@ -14,7 +14,7 @@ class Side(Enum):
 @dataclass
 class CubeDrawOptions:
     fill : bool
-    line_op : float
+    line_col : Tuple[float,float,float,float] = (0,0,0,1)
     fill_col_top : Tuple[float,float,float,float] = (0,0,1,1)
     fill_col_left : Tuple[float,float,float,float] = (0,0,1,1)
     fill_col_front : Tuple[float,float,float,float] = (0,0,1,1)
@@ -33,11 +33,11 @@ def draw_cube_op(context, side: Side, options : CubeDrawOptions):
         context.set_source_rgba(fill_col[0],fill_col[1],fill_col[2],fill_col[3])
         context.fill_preserve()
         context.set_line_width(1.0)
-        context.set_source_rgba(0,0,0,options.line_op)
+        context.set_source_rgba(options.line_col[0],options.line_col[1],options.line_col[2],options.line_col[3])
         context.stroke()
     else:
         context.set_line_width(1.0)
-        context.set_source_rgba(0,0,0,options.line_op)
+        context.set_source_rgba(options.line_col[0],options.line_col[1],options.line_col[2],options.line_col[3])
         context.stroke()
 
 def draw_cube_top(context, x_translate, y_translate, x_top_left, y_top_left, z_width, options):
@@ -164,7 +164,7 @@ def draw_cube_and_selection_interior(context, x_translate, y_translate, box_dim,
         y_top_left_start=y_top_left_start + sel.z_idx*y_translate + sel.y_idx*box_dim,
         options=CubeDrawOptions(
             fill=True, 
-            line_op=1.0, 
+            line_col=(0,0,0,1.0), 
             fill_col_top=(0,0,1,0.5),
             fill_col_left=(0,0,0.7,0.5),
             fill_col_front=(0,0,0.4,0.5)
@@ -180,7 +180,171 @@ def draw_cube_and_selection_interior(context, x_translate, y_translate, box_dim,
         nz=nz, 
         x_top_left_start=x_top_left_start, 
         y_top_left_start=y_top_left_start,
-        options=CubeDrawOptions(fill=False, line_op=0.25)
+        options=CubeDrawOptions(fill=False, line_col=(0,0,0,0.25))
+        )
+
+def draw_cube_sel_wo_grid(context, x_translate, y_translate, box_dim, x_top_left_start, y_top_left_start, options: CubeDrawOptions, sel_size):
+    draw_cube(context, 
+        x_translate=sel_size*x_translate, 
+        y_translate=sel_size*y_translate, 
+        box_dim=sel_size*box_dim, 
+        nx=1, 
+        ny=1,
+        nz=1, 
+        x_top_left_start=x_top_left_start, 
+        y_top_left_start=y_top_left_start,
+        options=options
+        ) 
+
+def draw_cube_and_selection_top(context, x_translate, y_translate, box_dim, nx, ny, nz, x_top_left_start, y_top_left_start, sel: Selection):
+
+    options_grid = CubeDrawOptions(fill=False, line_col=(0,0,0,0.25))
+
+    # Draw back-left grid part
+    if sel.sel_size + sel.z_idx > 0:
+        draw_cube_left_grid(context,
+            x_translate=x_translate,
+            y_translate=y_translate, 
+            box_dim=box_dim, 
+            ny=ny, 
+            nz=sel.z_idx+sel.sel_size, 
+            x_top_left_start=x_top_left_start, 
+            y_top_left_start=y_top_left_start,
+            options=options_grid
+            )
+
+        draw_cube_top_grid(context, 
+            x_translate=x_translate,
+            y_translate=y_translate, 
+            box_dim=box_dim, 
+            nx=nx, 
+            nz=sel.z_idx+sel.sel_size, 
+            x_top_left_start=x_top_left_start,
+            y_top_left_start=y_top_left_start,
+            options=options_grid
+            )
+
+    # Draw selection
+    options_sel = CubeDrawOptions(
+        fill=True, 
+        line_col=(0,0,0,1.0), 
+        fill_col_top=(0,0,0.6,1),
+        fill_col_left=(0,0,1.0,1),
+        fill_col_front=(0,0,0.8,1)
+        )
+    draw_cube_sel_wo_grid(context, 
+        x_translate=x_translate, 
+        y_translate=y_translate, 
+        box_dim=box_dim, 
+        x_top_left_start=x_top_left_start + sel.z_idx*x_translate, 
+        y_top_left_start=y_top_left_start + sel.z_idx*y_translate + sel.y_idx*box_dim,
+        options=options_sel,
+        sel_size=sel.sel_size
+        )
+
+    # Draw front grid part
+    draw_cube(context, 
+        x_translate=x_translate, 
+        y_translate=y_translate, 
+        box_dim=box_dim, 
+        nx=nx, 
+        ny=ny,
+        nz=nz-max(sel.sel_size+sel.z_idx,0), 
+        x_top_left_start=x_top_left_start + max((sel.z_idx + sel.sel_size),0) * x_translate, 
+        y_top_left_start=y_top_left_start + max((sel.z_idx + sel.sel_size),0) * y_translate,
+        options=options_grid
+        )
+
+def draw_cube_and_selection_top_and_front(context, x_translate, y_translate, box_dim, nx, ny, nz, x_top_left_start, y_top_left_start, sel: Selection):
+    draw_cube(context, 
+        x_translate=x_translate, 
+        y_translate=y_translate, 
+        box_dim=box_dim, 
+        nx=nx, 
+        ny=ny,
+        nz=nz, 
+        x_top_left_start=x_top_left_start, 
+        y_top_left_start=y_top_left_start,
+        options=CubeDrawOptions(fill=False, line_col=(0,0,0,0.25))
+        )
+
+    draw_cube(context, 
+        x_translate=sel.sel_size*x_translate, 
+        y_translate=sel.sel_size*y_translate, 
+        box_dim=sel.sel_size*box_dim, 
+        nx=1, 
+        ny=1,
+        nz=1, 
+        x_top_left_start=x_top_left_start + sel.z_idx*x_translate, 
+        y_top_left_start=y_top_left_start + sel.z_idx*y_translate + sel.y_idx*box_dim,
+        options=CubeDrawOptions(
+            fill=True, 
+            line_col=(0,0,0,1.0), 
+            fill_col_top=(0,0,1,1),
+            fill_col_left=(0,0,0.7,1),
+            fill_col_front=(0,0,0.4,1)
+            )
+        )
+
+def draw_cube_and_selection_front(context, x_translate, y_translate, box_dim, nx, ny, nz, x_top_left_start, y_top_left_start, sel: Selection):
+
+    options_grid = CubeDrawOptions(fill=False, line_col=(0,0,0,0.25))
+
+    # Draw bottom grid part
+    if ny - sel.y_idx > 0:
+        bottom_x_top_left_start = x_top_left_start
+        bottom_y_top_left_start = y_top_left_start + max(sel.y_idx,0) * box_dim
+
+        draw_cube_left_grid(context,
+            x_translate=x_translate,
+            y_translate=y_translate, 
+            box_dim=box_dim, 
+            ny=ny-sel.y_idx, 
+            nz=nz, 
+            x_top_left_start=bottom_x_top_left_start, 
+            y_top_left_start=bottom_y_top_left_start,
+            options=options_grid
+            )
+
+        draw_cube_front_grid(context, 
+            box_dim=box_dim, 
+            nx=nx, 
+            ny=ny-sel.y_idx, 
+            x_top_left_start=bottom_x_top_left_start+nz*x_translate, 
+            y_top_left_start=bottom_y_top_left_start+nz*y_translate,
+            options=options_grid
+            )
+
+    # Draw selection
+    options_sel = CubeDrawOptions(
+        fill=True, 
+        line_col=(0,0,0,1.0), 
+        fill_col_top=(0,0,1,1),
+        fill_col_left=(0,0,0.7,1),
+        fill_col_front=(0,0,0.4,1)
+        )
+    draw_cube_sel_wo_grid(context, 
+        x_translate=x_translate, 
+        y_translate=y_translate, 
+        box_dim=box_dim, 
+        x_top_left_start=x_top_left_start + sel.z_idx*x_translate, 
+        y_top_left_start=y_top_left_start + sel.z_idx*y_translate + sel.y_idx*box_dim,
+        options=options_sel,
+        sel_size=sel.sel_size
+        )
+
+    # Draw top grid part
+    top_ny = min(sel.y_idx, ny)
+    draw_cube(context, 
+        x_translate=x_translate, 
+        y_translate=y_translate, 
+        box_dim=box_dim, 
+        nx=nx, 
+        ny=top_ny,
+        nz=nz, 
+        x_top_left_start=x_top_left_start, 
+        y_top_left_start=y_top_left_start,
+        options=CubeDrawOptions(fill=False, line_col=(0,0,0,0.25))
         )
 
 # creating a SVG surface
@@ -206,12 +370,12 @@ with cairo.SVGSurface("svg.svg", 1200, 1200) as surface:
     y_translate = 20
     box_dim = 50
     nx = 3
-    ny = 6
+    ny = 4
     nz = 6
     x_top_left_start = 100
     y_top_left_start = 100
 
-    draw_cube_and_selection_interior(context,
+    draw_cube_and_selection_top(context,
         x_translate=x_translate, 
         y_translate=y_translate, 
         box_dim=box_dim, 
@@ -221,8 +385,8 @@ with cairo.SVGSurface("svg.svg", 1200, 1200) as surface:
         x_top_left_start=x_top_left_start, 
         y_top_left_start=y_top_left_start, 
         sel=Selection(
-            y_idx=1,
-            z_idx=3,
+            y_idx=-1,
+            z_idx=-2,
             sel_size=3
         ))
 
